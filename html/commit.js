pieter/gitx

commit 62f6463ec4d76c9f93d83443297a515b782aab0d

Suppress useless svn repository ID e-mail addresses.
When there is no e-mail address available git-svn will substitute the loginname@svn-repository-ID, but this is confusing and useless to have linked/display.

html/commit.js
@@ -19,7 +19,10 @@ var Commit = Class.create({
 
 		var match = this.header.match(/\nauthor (.*) <(.*@.*)> ([0-9].*)/);
 		this.author_name = match[1];
-		this.author_email = match[2];
+		if (this.author_email =~ (/@[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\z/))
+			this.author_email = null;
+		else
+			this.author_email = match[2];
 		this.author_date = new Date(parseInt(match[3]) * 1000);
 
 		match = this.header.match(/\ncommitter (.*) <(.*@.*)> ([0-9].*)/);
@@ -48,7 +51,10 @@ var loadCommit = function() {
 	commit = new Commit(CommitObject);
 
 	$("commitID").innerHTML = commit.sha;
-	$("authorID").innerHTML = commit.author_name + " &lt;<a href='mailto:" + commit.author_email + "'>" + commit.author_email + "</a>&gt;";
+	if (commit.author_email)
+		$("authorID").innerHTML = commit.author_name + " &lt;<a href='mailto:" + commit.author_email + "'>" + commit.author_email + "</a>&gt;";
+	else
+		$("authorID").innerHTML = commit.author_name;
 	$("date").innerHTML = commit.author_date;
 	$("subjectID").innerHTML =CommitObject.subject;
 	

commit 1472487f00704c821a7ba7680a66309f2ce3dade

html/commit.js: Show E-mail addresses again.
This was broken in 62f6463ec4d76c9f93d83443297a515b782aab0d

html/commit.js
@@ -20,10 +20,9 @@ var Commit = Class.create({
 
 		var match = this.header.match(/\nauthor (.*) <(.*@.*)> ([0-9].*)/);
 		this.author_name = match[1];
-		if (this.author_email =~ (/@[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\z/))
-			this.author_email = null;
-		else
+		if (!(match[2].match(/@[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/)))
 			this.author_email = match[2];
+
 		this.author_date = new Date(parseInt(match[3]) * 1000);
 
 		match = this.header.match(/\ncommitter (.*) <(.*@.*)> ([0-9].*)/);