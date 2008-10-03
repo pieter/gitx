bae7566cde9b9bbcb99ac88eac4b313757a04950
Pieter de Bie <pdebie@ai.rug.nl>
2008-10-03 19:07:05 +0200

    HistoryView: Allow tripple-clicking on header to undo sorting

---
+++ PBUnsortableTableHeader.h
@@ -0,0 +1,18 @@
+//
+//  PBUnsortableTableHeader.h
+//  GitX
+//
+//  Created by Pieter de Bie on 03-10-08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PBUnsortableTableHeader : NSTableHeaderView {
+	IBOutlet NSArrayController *controller;
+	int clickCount;
+	int columnIndex;
+}
+
+@end
