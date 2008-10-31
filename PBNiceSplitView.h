77ad37a76a82b241e8310228be8d3b310ce7ee22
Pieter de Bie <pdebie@ai.rug.nl>
2008-10-31 20:36:18 +0100

    History/CommitView: Use a nice-looking splitview divider
    This uses images based on those in Mail.app to create a nice
    looking splitview (Hush, don't tell Apple!)

---
+++ PBNiceSplitView.h
@@ -0,0 +1,15 @@
+//
+//  PBNiceSplitView.h
+//  GitX
+//
+//  Created by Pieter de Bie on 31-10-08.
+//  Copyright 2008 Pieter de Bie. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PBNiceSplitView : NSSplitView {
+
+}
+
+@end
