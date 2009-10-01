339026981abb4d4f2b7168b59ec8954381599535
Pieter de Bie <pdebie@ai.rug.nl>
2009-10-01 21:55:40 +0200

    Update testing framework

--- test/framework.rb
+++ test/framework.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env macruby
+#!/usr/bin/env arch -i386 macruby
 
 require 'fileutils'
 
@@ -6,6 +6,7 @@ TEST_DIR = File.dirname(__FILE__)
 FRAMEWORK_DIR = File.join(TEST_DIR, "..", "build", "Debug", "GitXTesting.framework")
 
 framework FRAMEWORK_DIR
+framework 'Cocoa'
 
 TEST_TMP_DIR = File.join(TEST_DIR, "tmp")
 FileUtils.mkdir_p(TEST_TMP_DIR)
