0c0b764005e392da1dbec3e4c0749b5d45d6db40
Nathan Kinsinger <nkinsinger@brotherbard.com>
2010-02-06 13:06:27 -0700

    Remote progress sheet
    Add a progress sheet for remote operations that takes the arguments for a command and runs it in a task then shows a success or failure sheet.

---
+++ PBRemoteProgressSheet.h
@@ -0,0 +1,47 @@
+//
+//  PBRemoteProgressSheetController.h
+//  GitX
+//
+//  Created by Nathan Kinsinger on 12/6/09.
+//  Copyright 2009 Nathan Kinsinger. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+extern NSString * const kGitXProgressDescription;
+extern NSString * const kGitXProgressSuccessDescription;
+extern NSString * const kGitXProgressSuccessInfo;
+extern NSString * const kGitXProgressErrorDescription;
+extern NSString * const kGitXProgressErrorInfo;
+
+
+@class PBGitRepository;
+@class PBGitWindowController;
+
+@interface PBRemoteProgressSheet : NSWindowController {
+	PBGitWindowController *controller;
+
+	NSArray  *arguments;
+	NSString *title;
+	NSString *description;
+
+	NSTask    *gitTask;
+	NSInteger  returnCode;
+
+	NSTextField         *progressDescription;
+	NSProgressIndicator *progressIndicator;
+
+	NSTimer *taskTimer;
+}
+
++ (void) beginRemoteProgressSheetForArguments:(NSArray *)args title:(NSString *)theTitle description:(NSString *)theDescription inDir:(NSString *)dir windowController:(NSWindowController *)windowController;
+
++ (void) beginRemoteProgressSheetForArguments:(NSArray *)args title:(NSString *)theTitle description:(NSString *)theDescription  inRepository:(PBGitRepository *)repo;
+
+
+@property (assign) IBOutlet NSTextField         *progressDescription;
+@property (assign) IBOutlet NSProgressIndicator *progressIndicator;
+
+
+@end
