87f76fd5a17f8c6b6d2e2f281602a0cc5939faee
Nathan Kinsinger <nkinsinger@brotherbard.com>
2010-02-19 22:54:25 -0700

    Bugfix: fix the contextual menu and drag-n-drop of refs
    When the Subject column is not the first column in the git history table the contextual menu for refs and dragging and dropping refs don't work.
    
    Offset the mouse click's x value by the x origin of the Subject column.
    
    In PBRefController look up the column index of the subject column and check that it's the clicked column (don't assume it's the first one).

--- PBCommitList.m
+++ PBCommitList.m
@@ -61,8 +61,9 @@
 	int row = [self rowAtPoint:location];
 	int column = [self columnAtPoint:location];
 	PBGitRevisionCell *cell = (PBGitRevisionCell *)[self preparedCellAtColumn:column row:row];
+	NSRect cellFrame = [self frameOfCellAtColumn:column row:row];
 
-	int index = [cell indexAtX:location.x];
+	int index = [cell indexAtX:(location.x - cellFrame.origin.x)];
 	if (index == -1)
 		return [super dragImageForRowsWithIndexes:dragRows tableColumns:tableColumns event:dragEvent offset:dragImageOffset];
 
