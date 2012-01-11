46f201a63a5fe55f364757451cbc99092d32130c
Rowan James <rowanj@phere.net>
2012-01-11 19:37:22 +1100

    Wrap thread entry points in @autoreleasepool { … }

--- Classes/git/PBGitRevList.mm
+++ Classes/git/PBGitRevList.mm
@@ -95,141 +95,147 @@ using namespace std;
 
 - (void) walkRevisionListWithSpecifier:(PBGitRevSpecifier*)rev
 {
-	NSDate *start = [NSDate date];
-	NSDate *lastUpdate = [NSDate date];
-	NSMutableArray *revisions = [NSMutableArray array];
-	PBGitGrapher *g = [[PBGitGrapher alloc] initWithRepository:repository];
-	std::map<string, NSStringEncoding> encodingMap;
-	NSThread *currentThread = [NSThread currentThread];
-
-	NSString *formatString = @"--pretty=format:%H\01%e\01%aN\01%cN\01%s\01%P\01%at";
-	BOOL showSign = [rev hasLeftRight];
-
-	if (showSign)
-		formatString = [formatString stringByAppendingString:@"\01%m"];
-	
-	NSMutableArray *arguments = [NSMutableArray arrayWithObjects:@"log", @"-z", @"--topo-order", @"--children", formatString, nil];
-
-	if (!rev)
-		[arguments addObject:@"HEAD"];
-	else
-		[arguments addObjectsFromArray:[rev parameters]];
-
-	NSString *directory = rev.workingDirectory ? rev.workingDirectory.path : repository.fileURL.path;
-	NSTask *task = [PBEasyPipe taskForCommand:[PBGitBinary path] withArgs:arguments inDir:directory];
-	[task launch];
-	NSFileHandle *handle = [task.standardOutput fileHandleForReading];
-	
-	int fd = [handle fileDescriptor];
-	__gnu_cxx::stdio_filebuf<char> buf(fd, std::ios::in);
-	std::istream stream(&buf);
-
-	int num = 0;
-	while (true) {
-		if ([currentThread isCancelled])
-			break;
-
-		string sha;
-		if (!getline(stream, sha, '\1'))
-			break;
-
-		// From now on, 1.2 seconds
-		string encoding_str;
-		getline(stream, encoding_str, '\1');
-		NSStringEncoding encoding = NSUTF8StringEncoding;
-		if (encoding_str.length())
-		{
-			if (encodingMap.find(encoding_str) != encodingMap.end()) {
-				encoding = encodingMap[encoding_str];
-			} else {
-				encoding = CFStringConvertEncodingToNSStringEncoding(CFStringConvertIANACharSetNameToEncoding((__bridge CFStringRef)[NSString stringWithUTF8String:encoding_str.c_str()]));
-				encodingMap[encoding_str] = encoding;
-			}
-		}
-
-		git_oid oid;
-		git_oid_fromstr(&oid, sha.c_str());
-		PBGitCommit *newCommit = [PBGitCommit commitWithRepository:repository andSha:[PBGitSHA shaWithOID:oid]];
-
-		string author;
-		getline(stream, author, '\1');
-
-		string committer;
-		getline(stream, committer, '\1');
-
-		string subject;
-		getline(stream, subject, '\1');
-
-		string parentString;
-		getline(stream, parentString, '\1');
-		if (parentString.size() != 0)
-		{
-			if (((parentString.size() + 1) % 41) != 0) {
-				NSLog(@"invalid parents: %zu", parentString.size());
-				continue;
-			}
-			int nParents = (parentString.size() + 1) / 41;
-			NSMutableArray *parents = [NSMutableArray arrayWithCapacity:nParents];
-			int parentIndex;
-			for (parentIndex = 0; parentIndex < nParents; ++parentIndex)
-				[parents addObject:[PBGitSHA shaWithCString:parentString.substr(parentIndex * 41, 40).c_str()]];
-
-			[newCommit setParents:parents];
-		}
-
-		int time;
-		stream >> time;
-
-		[newCommit setSubject:[NSString stringWithCString:subject.c_str() encoding:encoding]];
-		[newCommit setAuthor:[NSString stringWithCString:author.c_str() encoding:encoding]];
-		[newCommit setCommitter:[NSString stringWithCString:committer.c_str() encoding:encoding]];
-		[newCommit setTimestamp:time];
+	@autoreleasepool {
+		NSDate *start = [NSDate date];
+		NSDate *lastUpdate = [NSDate date];
+		NSMutableArray *revisions = [NSMutableArray array];
+		PBGitGrapher *g = [[PBGitGrapher alloc] initWithRepository:repository];
+		std::map<string, NSStringEncoding> encodingMap;
+		NSThread *currentThread = [NSThread currentThread];
+		
+		NSString *formatString = @"--pretty=format:%H\01%e\01%aN\01%cN\01%s\01%P\01%at";
+		BOOL showSign = [rev hasLeftRight];
 		
 		if (showSign)
-		{
+			formatString = [formatString stringByAppendingString:@"\01%m"];
+		
+		NSMutableArray *arguments = [NSMutableArray arrayWithObjects:@"log",
+									 @"-z",
+									 @"--topo-order",
+									 @"--children",
+									 formatString, nil];
+		
+		if (!rev)
+			[arguments addObject:@"HEAD"];
+		else
+			[arguments addObjectsFromArray:[rev parameters]];
+		
+		NSString *directory = rev.workingDirectory ? rev.workingDirectory.path : repository.fileURL.path;
+		NSTask *task = [PBEasyPipe taskForCommand:[PBGitBinary path] withArgs:arguments inDir:directory];
+		[task launch];
+		NSFileHandle *handle = [task.standardOutput fileHandleForReading];
+		
+		int fd = [handle fileDescriptor];
+		__gnu_cxx::stdio_filebuf<char> buf(fd, std::ios::in);
+		std::istream stream(&buf);
+		
+		int num = 0;
+		while (true) {
+			if ([currentThread isCancelled])
+				break;
+			
+			string sha;
+			if (!getline(stream, sha, '\1'))
+				break;
+			
+			// From now on, 1.2 seconds
+			string encoding_str;
+			getline(stream, encoding_str, '\1');
+			NSStringEncoding encoding = NSUTF8StringEncoding;
+			if (encoding_str.length())
+			{
+				if (encodingMap.find(encoding_str) != encodingMap.end()) {
+					encoding = encodingMap[encoding_str];
+				} else {
+					encoding = CFStringConvertEncodingToNSStringEncoding(CFStringConvertIANACharSetNameToEncoding((__bridge CFStringRef)[NSString stringWithUTF8String:encoding_str.c_str()]));
+					encodingMap[encoding_str] = encoding;
+				}
+			}
+			
+			git_oid oid;
+			git_oid_fromstr(&oid, sha.c_str());
+			PBGitCommit *newCommit = [PBGitCommit commitWithRepository:repository andSha:[PBGitSHA shaWithOID:oid]];
+			
+			string author;
+			getline(stream, author, '\1');
+			
+			string committer;
+			getline(stream, committer, '\1');
+			
+			string subject;
+			getline(stream, subject, '\1');
+			
+			string parentString;
+			getline(stream, parentString, '\1');
+			if (parentString.size() != 0)
+			{
+				if (((parentString.size() + 1) % 41) != 0) {
+					NSLog(@"invalid parents: %zu", parentString.size());
+					continue;
+				}
+				int nParents = (parentString.size() + 1) / 41;
+				NSMutableArray *parents = [NSMutableArray arrayWithCapacity:nParents];
+				int parentIndex;
+				for (parentIndex = 0; parentIndex < nParents; ++parentIndex)
+					[parents addObject:[PBGitSHA shaWithCString:parentString.substr(parentIndex * 41, 40).c_str()]];
+				
+				[newCommit setParents:parents];
+			}
+			
+			int time;
+			stream >> time;
+			
+			[newCommit setSubject:[NSString stringWithCString:subject.c_str() encoding:encoding]];
+			[newCommit setAuthor:[NSString stringWithCString:author.c_str() encoding:encoding]];
+			[newCommit setCommitter:[NSString stringWithCString:committer.c_str() encoding:encoding]];
+			[newCommit setTimestamp:time];
+			
+			if (showSign)
+			{
+				char c;
+				stream >> c; // Remove separator
+				stream >> c;
+				if (c != '>' && c != '<' && c != '^' && c != '-')
+					NSLog(@"Error loading commits: sign not correct");
+				[newCommit setSign: c];
+			}
+			
 			char c;
-			stream >> c; // Remove separator
 			stream >> c;
-			if (c != '>' && c != '<' && c != '^' && c != '-')
-				NSLog(@"Error loading commits: sign not correct");
-			[newCommit setSign: c];
-		}
-
-		char c;
-		stream >> c;
-		if (c != '\0')
-			cout << "Error" << endl;
-
-		[revisions addObject: newCommit];
-		if (isGraphing)
-			[g decorateCommit:newCommit];
-
-		if (++num % 100 == 0) {
-			if ([[NSDate date] timeIntervalSinceDate:lastUpdate] > 0.1) {
-				NSDictionary *update = [NSDictionary dictionaryWithObjectsAndKeys:currentThread, kRevListThreadKey, revisions, kRevListRevisionsKey, nil];
-				[self performSelectorOnMainThread:@selector(updateCommits:) withObject:update waitUntilDone:NO];
-				revisions = [NSMutableArray array];
-				lastUpdate = [NSDate date];
+			if (c != '\0')
+				cout << "Error" << endl;
+			
+			[revisions addObject: newCommit];
+			if (isGraphing)
+				[g decorateCommit:newCommit];
+			
+			if (++num % 100 == 0) {
+				if ([[NSDate date] timeIntervalSinceDate:lastUpdate] > 0.1) {
+					NSDictionary *update = [NSDictionary dictionaryWithObjectsAndKeys:currentThread, kRevListThreadKey, revisions, kRevListRevisionsKey, nil];
+					[self performSelectorOnMainThread:@selector(updateCommits:) withObject:update waitUntilDone:NO];
+					revisions = [NSMutableArray array];
+					lastUpdate = [NSDate date];
+				}
 			}
 		}
+		
+		if (![currentThread isCancelled]) {
+			NSTimeInterval duration = [[NSDate date] timeIntervalSinceDate:start];
+			NSLog(@"Loaded %i commits in %f seconds (%f/sec)", num, duration, num/duration);
+			
+			// Make sure the commits are stored before exiting.
+			NSDictionary *update = [NSDictionary dictionaryWithObjectsAndKeys:currentThread, kRevListThreadKey, revisions, kRevListRevisionsKey, nil];
+			[self performSelectorOnMainThread:@selector(updateCommits:) withObject:update waitUntilDone:YES];
+			
+			[self performSelectorOnMainThread:@selector(finishedParsing) withObject:nil waitUntilDone:NO];
+		}
+		else {
+			NSLog(@"[%@ %@] thread has been canceled", [self class], NSStringFromSelector(_cmd));
+		}
+		
+		[task terminate];
+		[task waitUntilExit];
 	}
-	
-	if (![currentThread isCancelled]) {
-		NSTimeInterval duration = [[NSDate date] timeIntervalSinceDate:start];
-		NSLog(@"Loaded %i commits in %f seconds (%f/sec)", num, duration, num/duration);
-
-		// Make sure the commits are stored before exiting.
-		NSDictionary *update = [NSDictionary dictionaryWithObjectsAndKeys:currentThread, kRevListThreadKey, revisions, kRevListRevisionsKey, nil];
-		[self performSelectorOnMainThread:@selector(updateCommits:) withObject:update waitUntilDone:YES];
-
-		[self performSelectorOnMainThread:@selector(finishedParsing) withObject:nil waitUntilDone:NO];
-	}
-	else {
-		NSLog(@"[%@ %@] thread has been canceled", [self class], NSStringFromSelector(_cmd));
-	}
-
-	[task terminate];
-	[task waitUntilExit];
 }
 
 @end
--- Classes/main.m
+++ Classes/main.m
@@ -8,7 +8,11 @@
 
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, char *argv[])
+int main(int argc, const char **argv)
 {
-    return NSApplicationMain(argc,  (const char **) argv);
+	int result;
+	@autoreleasepool {
+		result = NSApplicationMain(argc, argv);
+	}
+	return result;
 }
