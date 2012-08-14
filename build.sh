7707fa373b811e7b008cdb5121fb87c068430f7a
Rowan James <rowanj@phere.net>
2012-08-14 12:16:12 +1000

    Add some sophistication to `build.sh`
    Usage and code should be self-documenting enough.
    Changes relate to issue #67

--- build.sh
+++ build.sh
@@ -1,3 +1,59 @@
 #!/bin/bash
 
-xcrun xcodebuild -scheme GitX -workspace GitX.xcodeproj/project.xcworkspace -configuration Release build
+# enter the directory the script is in
+cd "`dirname $0`"
+
+scheme="GitX"
+workspace="${scheme}.xcodeproj/project.xcworkspace"
+configuration=""
+clean=""
+pause=10
+
+print_usage()
+{
+    echo "Usage: `basename $0` [-c] -d|-r"
+    echo "Where   -c clean before building"
+    echo "        -d build for Debug configuration"
+    echo "        -r build for Release configuration"
+}
+
+while getopts drch flag
+    do
+        case $flag in
+
+            d)
+		configuration="Debug"
+                  ;;
+            r)
+		configuration="Release"
+                 ;;
+	    c)
+		clean="clean"
+		;;
+	    h)
+		print_usage
+		exit
+		;;
+            ?)
+		print_usage
+                exit
+                ;;
+        esac
+   done
+shift $(( OPTIND - 1 ))  # shift past the last flag or argument
+
+if [ -z "$configuration" ]; then
+    echo "`basename $0`: error: must specify -d or -r"
+    print_usage
+    exit 1
+fi
+
+if [ -z "$clean" ];
+then
+    echo "Building $scheme from $workspace for $configuration in ${pause}..."
+else
+    echo "Building $scheme (cleanly) from $workspace for $configuration in ${pause}..."
+fi
+sleep $pause
+
+xcrun xcodebuild -scheme "$scheme" -workspace "$workspace" -configuration "$configuration" "$clean" build CONFIGURATION_BUILD_DIR="`pwd`/build/${configuration}"
