dc6a728e6b889e98c0cd3a5458f4fc61e63430f7
Mathias Leppich <mleppich@muhqu.de>
2013-08-21 22:51:50 +0200

    improve tokenizer performance by using single regex  \o/

--- html/lib/diffHighlighter.js
+++ html/lib/diffHighlighter.js
@@ -407,22 +407,7 @@ var inlinediff = (function () {
   }
 
   function whitespaceAwareTokenize(n) {
-    var nz = [], nn = n === "" ? [] : (n.match(/\n|[^\n]+/g) || []);
-    for (var i = 0; i < nn.length; i++) {
-        if (nn[i] == "\n") {
-            nz.push(nn[i]);
-        }
-        else {
-            var ns = (nn[i].match(/\s+|[^\s]+/g) || []);
-            for (var k = 0; k < ns.length; k++) {
-                var nw = (ns[k].match(/\s+|\W|\w+/g) || []);
-                for (var j = 0; j < nw.length; j++) {
-                    nz.push(nw[j]);
-                }
-            }
-        }
-    }
-    return nz;
+    return n !== "" && n.match(/\n| *[\-><!=]+ *|[ \t]+|[<$&#§%]\w+|\w+|\W/g) || [];
   }
 
   function tag(t,c) {
