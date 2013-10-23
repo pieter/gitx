5b80e3c14374015a03306d7205cb49be5d4ec970
Etienne Samson <tiennou7@gmail.com>
2013-10-23 20:17:45 +0200

    Rename the DEPRECATED macro to GITX_DEPRECATED, and wrap it in a #define
    So it can be temporarily hidden.

--- Classes/Util/PBEasyPipe.h
+++ Classes/Util/PBEasyPipe.h
@@ -12,28 +12,28 @@
 
 }
 
-+ (NSTask *) taskForCommand:(NSString *)cmd withArgs:(NSArray *)args inDir:(NSString *)dir DEPRECATED;
++ (NSTask *) taskForCommand:(NSString *)cmd withArgs:(NSArray *)args inDir:(NSString *)dir GITX_DEPRECATED;
 
-+ (NSFileHandle*) handleForCommand: (NSString*) cmd withArgs: (NSArray*) args DEPRECATED;
-+ (NSFileHandle*) handleForCommand: (NSString*) cmd withArgs: (NSArray*) args inDir: (NSString*) dir DEPRECATED;
++ (NSFileHandle*) handleForCommand: (NSString*) cmd withArgs: (NSArray*) args GITX_DEPRECATED;
++ (NSFileHandle*) handleForCommand: (NSString*) cmd withArgs: (NSArray*) args inDir: (NSString*) dir GITX_DEPRECATED;
 
-+ (NSString*) outputForCommand: (NSString*) cmd withArgs: (NSArray*) args DEPRECATED;
-+ (NSString*) outputForCommand: (NSString*) cmd withArgs: (NSArray*) args inDir: (NSString*) dir DEPRECATED;
++ (NSString*) outputForCommand: (NSString*) cmd withArgs: (NSArray*) args GITX_DEPRECATED;
++ (NSString*) outputForCommand: (NSString*) cmd withArgs: (NSArray*) args inDir: (NSString*) dir GITX_DEPRECATED;
 + (NSString*) outputForCommand:(NSString *) cmd
 					  withArgs:(NSArray *)  args
 						 inDir:(NSString *) dir
-				      retValue:(int *)      ret DEPRECATED;
+				      retValue:(int *)      ret GITX_DEPRECATED;
 + (NSString*) outputForCommand:(NSString *) cmd
 					  withArgs:(NSArray *)  args
 						 inDir:(NSString *) dir
 				   inputString:(NSString *)input
-				      retValue:(int *)      ret DEPRECATED;
+				      retValue:(int *)      ret GITX_DEPRECATED;
 + (NSString*) outputForCommand:(NSString *) cmd
 					  withArgs:(NSArray *)  args
 						 inDir:(NSString *) dir
 		byExtendingEnvironment:(NSDictionary *)dict
 				   inputString:(NSString *)input
-				      retValue:(int *)      ret DEPRECATED;
+				      retValue:(int *)      ret GITX_DEPRECATED;
 
 
 @end
