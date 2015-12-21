pieter/gitx

commit 4735b1b2956f6bda4cc0610005d552ad3b4a851e

Improve handling of multiple item selections
Displays a message in the diff area instead of the commit selected
before creating a multiple-commit selectction. And takes care of
disabling main/contextual menu items when multiple commits are selected.

We now have to pass a list of selected commits around rather than a
single commit as before, which means a number of classes had to be
modified.

Also tries to re-use the same logic for clipboard operations in the main
menu and contextual menus.

Classes/Controllers/PBRefController.m
@@ -15,6 +15,7 @@
 #import "PBDiffWindowController.h"
 #import "PBGitRevSpecifier.h"
 #import "PBGitStash.h"
+#import "GitXCommitCopier.h"
 
 #define kDialogAcceptDroppedRef @"Accept Dropped Ref"
 #define kDialogConfirmPush @"Confirm Push"
@@ -34,7 +35,7 @@
 
 - (void) fetchRemote:(PBRefMenuItem *)sender
 {
-	id <PBGitRefish> refish = [sender refish];
+	id <PBGitRefish> refish = sender.refishs.firstObject;
 	if ([refish refishType] == kGitXCommitType)
 		return;
 
@@ -46,7 +47,7 @@
 
 - (void) pullRemote:(PBRefMenuItem *)sender
 {
-	id <PBGitRefish> refish = [sender refish];
+	id <PBGitRefish> refish = sender.refishs.firstObject;
 	[historyController.repository beginPullFromRemote:nil forRef:refish];
 }
 
@@ -110,21 +111,20 @@
 
 - (void) pushUpdatesToRemote:(PBRefMenuItem *)sender
 {
-	PBGitRef *remoteRef = [(PBGitRef *)[sender refish] remoteRef];
+	PBGitRef *remoteRef = [(PBGitRef *)[sender refishs] remoteRef];
 
 	[self showConfirmPushRefSheet:nil remote:remoteRef];
 }
 
 - (void) pushDefaultRemoteForRef:(PBRefMenuItem *)sender
 {
-	PBGitRef *ref = (PBGitRef *)[sender refish];
-
+	PBGitRef *ref = sender.refishs.firstObject;
 	[self showConfirmPushRefSheet:ref remote:nil];
 }
 
 - (void) pushToRemote:(PBRefMenuItem *)sender
 {
-	PBGitRef *ref = (PBGitRef *)[sender refish];
+	PBGitRef *ref = sender.refishs.firstObject;;
 	NSString *remoteName = [sender representedObject];
 	PBGitRef *remoteRef = [PBGitRef refFromString:[kGitXRemoteRefPrefix stringByAppendingString:remoteName]];
 
@@ -136,7 +136,7 @@
 
 - (void) merge:(PBRefMenuItem *)sender
 {
-	id <PBGitRefish> refish = [sender refish];
+	id <PBGitRefish> refish = sender.refishs.firstObject;
 	[historyController.repository mergeWithRefish:refish];
 }
 
@@ -145,7 +145,7 @@
 
 - (void) checkout:(PBRefMenuItem *)sender
 {
-	id <PBGitRefish> refish = [sender refish];
+	id <PBGitRefish> refish = sender.refishs.firstObject;
 	[historyController.repository checkoutRefish:refish];
 }
 
@@ -154,7 +154,7 @@
 
 - (void) cherryPick:(PBRefMenuItem *)sender
 {
-	id <PBGitRefish> refish = [sender refish];
+	id <PBGitRefish> refish = sender.refishs.firstObject;
 	[historyController.repository cherryPickRefish:refish];
 }
 
@@ -163,8 +163,7 @@
 
 - (void) rebaseHeadBranch:(PBRefMenuItem *)sender
 {
-	id <PBGitRefish> refish = [sender refish];
-
+	id <PBGitRefish> refish = sender.refishs.firstObject;
 	[historyController.repository rebaseBranch:nil onRefish:refish];
 }
 
@@ -173,7 +172,7 @@
 
 - (void) createBranch:(PBRefMenuItem *)sender
 {
-	id <PBGitRefish> refish = [sender refish];
+	id <PBGitRefish> refish = sender.refishs.firstObject;
 	[PBCreateBranchSheet beginCreateBranchSheetAtRefish:refish inRepository:historyController.repository];
 }
 
@@ -182,43 +181,32 @@
 
 - (void) copySHA:(PBRefMenuItem *)sender
 {
-	PBGitCommit *commit = nil;
-	if ([[sender refish] refishType] == kGitXCommitType)
-		commit = (PBGitCommit *)[sender refish];
-	else
-		commit = [historyController.repository commitForRef:[sender refish]];
-
-	NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
-	[pasteboard declareTypes:[NSArray arrayWithObject:NSStringPboardType] owner:nil];
-	[pasteboard setString:commit.SHA forType:NSStringPboardType];
+	[GitXCommitCopier putStringToPasteboard:[GitXCommitCopier toFullSHA:[self commitsForMenuItem:sender]]];
 }
-
 
 - (void) copyShortSHA:(PBRefMenuItem *)sender
 {
-	PBGitCommit *commit = nil;
-	if ([[sender refish] refishType] == kGitXCommitType)
-		commit = (PBGitCommit *)[sender refish];
-	else
-		commit = [historyController.repository commitForRef:[sender refish]];
-    
-	NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
-	[pasteboard declareTypes:[NSArray arrayWithObject:NSStringPboardType] owner:nil];
-	[pasteboard setString:[commit shortName] forType:NSStringPboardType];
+	[GitXCommitCopier putStringToPasteboard:[GitXCommitCopier toShortName:[self commitsForMenuItem:sender]]];
 }
-
 
 - (void) copyPatch:(PBRefMenuItem *)sender
 {
-	PBGitCommit *commit = nil;
-	if ([[sender refish] refishType] == kGitXCommitType)
-		commit = (PBGitCommit *)[sender refish];
-	else
-		commit = [historyController.repository commitForRef:[sender refish]];
+	[GitXCommitCopier putStringToPasteboard:[GitXCommitCopier toPatch:[self commitsForMenuItem:sender]]];
+}
 
-	NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
-	[pasteboard declareTypes:[NSArray arrayWithObject:NSStringPboardType] owner:nil];
-	[pasteboard setString:[commit patch] forType:NSStringPboardType];
+- (NSArray<PBGitCommit *> *) commitsForMenuItem:(PBRefMenuItem *)menuItem {
+	NSArray<id<PBGitRefish>> * refishs = menuItem.refishs;
+	NSMutableArray *commits = [NSMutableArray arrayWithCapacity:refishs.count];
+	for (id<PBGitRefish> refish in refishs) {
+		[commits addObject:[self refishToCommit:refish]];
+	}
+	return commits;
+}
+
+- (PBGitCommit *) refishToCommit:(id<PBGitRefish>)refish {
+	return [refish refishType] == kGitXCommitType
+		? (PBGitCommit *)refish
+		: [historyController.repository commitForRef:refish];
 }
 
 
@@ -226,47 +214,42 @@
 
 - (void) diffWithHEAD:(PBRefMenuItem *)sender
 {
-	PBGitCommit *commit = nil;
-	if ([[sender refish] refishType] == kGitXCommitType)
-		commit = (PBGitCommit *)[sender refish];
-	else
-		commit = [historyController.repository commitForRef:[sender refish]];
-
+	PBGitCommit *commit = [self commitsForMenuItem:sender].firstObject;
 	[PBDiffWindowController showDiffWindowWithFiles:nil fromCommit:commit diffCommit:nil];
 }
 
 #pragma mark Stash
 
--(void) stashPop:(id)sender
+-(void) stashPop:(PBRefMenuItem *)sender
 {
-    PBGitStash * stash = [historyController.repository stashForRef:[sender refish]];
+    PBGitStash * stash = [historyController.repository stashForRef:[sender refishs].firstObject];
     BOOL ok = [historyController.repository stashPop:stash];
     if (ok) {
         [historyController.repository.windowController showCommitView:sender];
     }
 }
 
--(void) stashApply:(id)sender
+-(void) stashApply:(PBRefMenuItem *)sender
 {
-    PBGitStash * stash = [historyController.repository stashForRef:[sender refish]];
+    PBGitStash * stash = [historyController.repository stashForRef:[sender refishs].firstObject];
     BOOL ok = [historyController.repository stashApply:stash];
     if (ok) {
         [historyController.repository.windowController showCommitView:sender];
     }
 }
 
--(void) stashDrop:(id)sender
+-(void) stashDrop:(PBRefMenuItem *)sender
 {
-    PBGitStash * stash = [historyController.repository stashForRef:[sender refish]];
+    PBGitStash * stash = [historyController.repository stashForRef:[sender refishs].firstObject];
     BOOL ok = [historyController.repository stashDrop:stash];
     if (ok) {
         [historyController.repository.windowController showHistoryView:sender];
     }
 }
 
--(void) stashViewDiff:(id)sender
+-(void) stashViewDiff:(PBRefMenuItem *)sender
 {
-    PBGitStash * stash = [historyController.repository stashForRef:[sender refish]];
+    PBGitStash * stash = [historyController.repository stashForRef:sender.refishs.firstObject];
     [PBDiffWindowController showDiffWindowWithFiles:nil fromCommit:stash.ancestorCommit diffCommit:stash.commit];
 }
 
@@ -274,18 +257,21 @@
 
 - (void) createTag:(PBRefMenuItem *)sender
 {
-	id <PBGitRefish> refish = [sender refish];
+	id <PBGitRefish> refish = [sender refishs].firstObject;
 	[PBCreateTagSheet beginCreateTagSheetAtRefish:refish inRepository:historyController.repository];
 }
 
 - (void) showTagInfoSheet:(PBRefMenuItem *)sender
 {
-	if ([[sender refish] refishType] != kGitXTagType)
+	id<PBGitRefish> refish = sender.refishs.firstObject;
+	if ([refish refishType] != kGitXTagType)
 		return;
 
-	NSError *error = nil;
-	NSString *tagName = [(PBGitRef *)[sender refish] tagName];
+	PBGitRef *ref = (PBGitRef *)refish;
+
+	NSString *tagName = [ref tagName];
 	NSString *tagRef = [@"refs/tags/" stringByAppendingString:tagName];
+	NSError *error = nil;
 	GTObject *object = [historyController.repository.gtRepo lookUpObjectByRevParse:tagRef error:&error];
 	if (!object) {
 		NSLog(@"Couldn't look up ref %@:%@", tagRef, [error debugDescription]);
@@ -305,10 +291,11 @@
 
 - (void)showDeleteRefSheet:(PBRefMenuItem *)sender
 {
-	if ([[sender refish] refishType] == kGitXCommitType)
+	id<PBGitRefish> refish = sender.refishs.firstObject;
+	if ([refish refishType] == kGitXCommitType)
 		return;
 
-	PBGitRef *ref = (PBGitRef *)[sender refish];
+	PBGitRef *ref = (PBGitRef *)refish;
 
 	if ([PBGitDefaults isDialogWarningSuppressedForDialog:kDialogDeleteRef]) {
 		[historyController.repository deleteRef:ref];
@@ -347,24 +334,25 @@
 
 #pragma mark Contextual menus
 
-- (NSArray *) menuItemsForRef:(PBGitRef *)ref
+- (NSArray<NSMenuItem *> *) menuItemsForRef:(PBGitRef *)ref
 {
 	return [PBRefMenuItem defaultMenuItemsForRef:ref inRepository:historyController.repository target:self];
 }
 
-- (NSArray *) menuItemsForCommit:(PBGitCommit *)commit
+- (NSArray<NSMenuItem *> *) menuItemsForCommits:(NSArray<PBGitCommit *> *)commits
 {
-	return [PBRefMenuItem defaultMenuItemsForCommit:commit target:self];
+	return [PBRefMenuItem defaultMenuItemsForCommits:commits target:self];
 }
 
-- (NSArray *)menuItemsForRow:(NSInteger)rowIndex
+- (NSArray<NSMenuItem *> *)menuItemsForRow:(NSInteger)rowIndex
 {
-	NSArray *commits = [commitController arrangedObjects];
+	NSArray<PBGitCommit *> *commits = [commitController arrangedObjects];
 	if ([commits count] <= rowIndex)
 		return nil;
 
-	return [self menuItemsForCommit:[commits objectAtIndex:rowIndex]];
+	return [self menuItemsForCommits:@[[commits objectAtIndex:rowIndex]]];
 }
+
 
 
 # pragma mark Tableview delegate methods

Classes/Controllers/PBGitHistoryController.h
@@ -36,9 +36,6 @@
 	__weak IBOutlet PBCommitList *commitList;
 	__weak IBOutlet PBCollapsibleSplitView *historySplitView;
 	__weak IBOutlet PBGitGradientBarView *upperToolbarView;
-	__weak IBOutlet NSButton *mergeButton;
-	__weak IBOutlet NSButton *cherryPickButton;
-	__weak IBOutlet NSButton *rebaseButton;
 	__weak IBOutlet PBGitGradientBarView *scopeBarView;
 	__weak IBOutlet NSButton *allBranchesFilterItem;
 	__weak IBOutlet NSButton *localRemoteBranchesFilterItem;
@@ -46,22 +43,27 @@
 	__weak IBOutlet id webView;
 
 	NSArray *currentFileBrowserSelectionPath;
-    QLPreviewPanel* previewPanel;
+    QLPreviewPanel *previewPanel;
 	int selectedCommitDetailsIndex;
 	BOOL forceSelectionUpdate;
 	PBGitTree *gitTree;
-	PBGitCommit *webCommit;
-	PBGitCommit *selectedCommit;
+	NSArray<PBGitCommit *> *webCommits;
+	NSArray<PBGitCommit *> *selectedCommits;
 }
 
-@property (readonly) NSTreeController* treeController;
-@property (assign) int selectedCommitDetailsIndex;
-@property  PBGitCommit *webCommit;
-@property  PBGitTree* gitTree;
 @property (readonly) NSArrayController *commitController;
+@property (readonly) NSTreeController *treeController;
 @property (readonly) PBRefController *refController;
 @property (readonly) PBHistorySearchController *searchController;
+
+@property (assign) int selectedCommitDetailsIndex;
+@property PBGitTree* gitTree;
+@property NSArray<PBGitCommit *> *webCommits;
+@property NSArray<PBGitCommit *> *selectedCommits;
+
 @property (readonly) PBCommitList *commitList;
+@property (readonly) BOOL singleCommitSelected;
+@property (readonly) BOOL singleNonHeadCommitSelected;
 
 - (IBAction) setDetailedView:(id)sender;
 - (IBAction) setTreeView:(id)sender;
@@ -94,6 +96,9 @@
 
 - (void) copyCommitInfo;
 - (void) copyCommitSHA;
+- (void) copyCommitShortName;
+- (void) copyCommitPatch;
+
 
 - (BOOL) hasNonlinearPath;
 

Classes/Controllers/PBGitHistoryController.m
@@ -31,6 +31,7 @@
 #define QLPreviewPanel NSClassFromString(@"QLPreviewPanel")
 #import "PBQLTextView.h"
 #import "GLFileView.h"
+#import "GitXCommitCopier.h"
 
 
 #define kHistorySelectedDetailIndexKey @"PBHistorySelectedDetailIndex"
@@ -50,10 +51,11 @@
 
 
 @implementation PBGitHistoryController
-@synthesize webCommit, gitTree, commitController, refController;
+@synthesize webCommits, gitTree, commitController, refController;
 @synthesize searchController;
 @synthesize commitList;
 @synthesize treeController;
+@synthesize selectedCommits;
 
 - (void)awakeFromNib
 {
@@ -122,34 +124,44 @@
     }
 }
 
-- (void)updateKeys
+- (void) updateKeys
 {
-	PBGitCommit *lastObject = [[commitController selectedObjects] lastObject];
-	if (lastObject) {
-		if (![selectedCommit isEqual:lastObject]) {
-			selectedCommit = lastObject;
-
-			BOOL isOnHeadBranch = [selectedCommit isOnHeadBranch];
-			[mergeButton setEnabled:!isOnHeadBranch];
-			[cherryPickButton setEnabled:!isOnHeadBranch];
-			[rebaseButton setEnabled:!isOnHeadBranch];
-		}
+	NSArray<PBGitCommit *> *newSelectedCommits = commitController.selectedObjects;
+	if  (![self.selectedCommits isEqualToArray:newSelectedCommits]) {
+		self.selectedCommits = newSelectedCommits;
 	}
-	else {
-		[mergeButton setEnabled:NO];
-		[cherryPickButton setEnabled:NO];
-		[rebaseButton setEnabled:NO];
-	}
-
+	
+	PBGitCommit *firstSelectedCommit = self.selectedCommits.firstObject;
+	
 	if (self.selectedCommitDetailsIndex == kHistoryTreeViewIndex) {
-		self.gitTree = selectedCommit.tree;
+		self.gitTree = firstSelectedCommit.tree;
 		[self restoreFileBrowserSelection];
 	}
 	else {
 		// kHistoryDetailViewIndex
-		if (![self.webCommit isEqual:selectedCommit])
-		self.webCommit = selectedCommit;
+		if (![self.webCommits isEqualToArray:self.selectedCommits]) {
+			self.webCommits = self.selectedCommits;
+		}
 	}
+}
+
+- (BOOL) singleCommitSelected
+{
+	return self.selectedCommits.count == 1;
+}
+
++ (NSSet *) keyPathsForValuesAffectingSingleCommitSelected {
+	return [NSSet setWithObjects:@"selectedCommits", nil];
+}
+
+- (BOOL) singleNonHeadCommitSelected
+{
+	return self.singleCommitSelected
+		&& ![self.selectedCommits.firstObject isOnHeadBranch];
+}
+
++ (NSSet *) keyPathsForValuesAffectingSingleNonHeadCommitSelected {
+	return [self keyPathsForValuesAffectingSingleCommitSelected];
 }
 
 - (void) updateBranchFilterMatrix
@@ -190,7 +202,7 @@
 
 - (BOOL)isCommitSelected
 {
-	return [selectedCommit isEqual:[[commitController selectedObjects] lastObject]];
+	return [self.selectedCommits isEqualToArray:[commitController selectedObjects]];
 }
 
 - (void) setSelectedCommitDetailsIndex:(int)detailsIndex
@@ -317,15 +329,22 @@
 
 - (BOOL)validateMenuItem:(NSMenuItem *)menuItem
 {
-    if ([menuItem action] == @selector(setDetailedView:)) {
+	SEL action = menuItem.action;
+
+    if (action == @selector(setDetailedView:)) {
 		[menuItem setState:(self.selectedCommitDetailsIndex == kHistoryDetailViewIndex) ? NSOnState : NSOffState];
-    } else if ([menuItem action] == @selector(setTreeView:)) {
+    } else if (action == @selector(setTreeView:)) {
 		[menuItem setState:(self.selectedCommitDetailsIndex == kHistoryTreeViewIndex) ? NSOnState : NSOffState];
-    }
-
-    if ([self respondsToSelector:[menuItem action]])
+	} 
+	
+	if ([self respondsToSelector:action]) {
+		if (action == @selector(createBranch:) || action == @selector(createTag:)) {
+			return self.singleCommitSelected;
+		}
+		
         return YES;
-
+	}
+	
     return [[self nextResponder] validateMenuItem:menuItem];
 }
 
@@ -383,35 +402,25 @@
 
 - (void) copyCommitInfo
 {
-	NSArray * strings = [self selectedCommitsWithTransformation:^(PBGitCommit * commit) {
-		return [NSString stringWithFormat:@"%@ (%@)", [commit.SHA substringToIndex:10], commit.subject];
-	}];
-	[self.class putStringToPasteboard:[strings componentsJoinedByString:@"\n"]];
+	[GitXCommitCopier putStringToPasteboard:[GitXCommitCopier toSHAAndHeadingString:commitController.selectedObjects]];
 }
 
 - (void) copyCommitSHA
 {
-	NSArray * strings = [self selectedCommitsWithTransformation:^(PBGitCommit * commit) { return commit.shortName; }];
-	[self.class putStringToPasteboard:[strings componentsJoinedByString:@" "]];
+	[GitXCommitCopier putStringToPasteboard:[GitXCommitCopier toFullSHA:commitController.selectedObjects]];
 }
 
-- (NSArray *) selectedCommitsWithTransformation:(NSString *(^)(PBGitCommit * _Nonnull commit))transformer
+- (void) copyCommitShortName
 {
-	NSArray *commits = commitController.selectedObjects;
-	NSMutableArray *shortNames = [NSMutableArray arrayWithCapacity:commits.count];
-	[commits enumerateObjectsUsingBlock:^(PBGitCommit * _Nonnull commit, NSUInteger idx, BOOL * _Nonnull stop) {
-		[shortNames addObject:transformer(commit)];
-	}];
-	return shortNames;
+	[GitXCommitCopier putStringToPasteboard:[GitXCommitCopier toShortName:commitController.selectedObjects]];
 }
 
-+ (void) putStringToPasteboard:(NSString *)string {
-	if (string.length > 0) {
-		NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
-		[pasteboard declareTypes:@[NSStringPboardType] owner:self];
-		[pasteboard setString:string forType:NSStringPboardType];
-	}
+- (void) copyCommitPatch
+{
+	[GitXCommitCopier putStringToPasteboard:[GitXCommitCopier toPatch:commitController.selectedObjects]];
 }
+
+
 
 - (IBAction) toggleQLPreviewPanel:(id)sender
 {
@@ -500,24 +509,25 @@
 - (NSArray *) selectedObjectsForOID:(GTOID *)commitOID
 {
 	NSPredicate *selection = [NSPredicate predicateWithFormat:@"OID == %@", commitOID];
-	NSArray *selectedCommits = [[commitController content] filteredArrayUsingPredicate:selection];
+	NSArray *selectionCommits = [[commitController content] filteredArrayUsingPredicate:selection];
 
-	if (([selectedCommits count] == 0) && [self firstCommit])
-		selectedCommits = [NSArray arrayWithObject:[self firstCommit]];
-
-	return selectedCommits;
+	if ((selectionCommits.count == 0) && [self firstCommit] != nil) {
+		selectionCommits = @[[self firstCommit]];
+	}
+	
+	return selectionCommits;
 }
 
 - (void)selectCommit:(GTOID *)commitOID
 {
-	if (!forceSelectionUpdate && [[[commitController.selectedObjects lastObject] OID] isEqual:commitOID])
+	if (!forceSelectionUpdate && [[[commitController.selectedObjects lastObject] OID] isEqual:commitOID]) {
 		return;
+	}
+
+	NSArray *selectedObjects = [self selectedObjectsForOID:commitOID];
+	[commitController setSelectedObjects:selectedObjects];
 
 	NSInteger oldIndex = [[commitController selectionIndexes] firstIndex];
-
-	NSArray *selectedCommits = [self selectedObjectsForOID:commitOID];
-	[commitController setSelectedObjects:selectedCommits];
-
 	[self scrollSelectionToTopOfViewFrom:oldIndex];
 
 	forceSelectionUpdate = NO;
@@ -579,13 +589,12 @@
 	NSMutableArray *files = [NSMutableArray array];
 	for (NSString *filePath in [sender representedObject])
 		[files addObject:[filePath stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]];
-
-	[repository checkoutFiles:files fromRefish:selectedCommit];
+	[repository checkoutFiles:files fromRefish:self.selectedCommits.firstObject];
 }
 
 - (void) diffFilesAction:(id)sender
 {
-	[PBDiffWindowController showDiffWindowWithFiles:[sender representedObject] fromCommit:selectedCommit diffCommit:nil];
+	[PBDiffWindowController showDiffWindowWithFiles:[sender representedObject] fromCommit:self.selectedCommits.firstObject diffCommit:nil];
 }
 
 - (NSMenu *)contextMenuForTreeView
@@ -612,7 +621,7 @@
 	PBGitRef *headRef = [[repository headRef] ref];
 	NSString *headRefName = [headRef shortName];
 	NSString *diffTitle = [NSString stringWithFormat:@"Diff %@ with %@", multiple ? @"files" : @"file", headRefName];
-	BOOL isHead = [selectedCommit.OID isEqual:repository.headOID];
+	BOOL isHead = [self.selectedCommits.firstObject.OID isEqual:repository.headOID];
 	NSMenuItem *diffItem = [[NSMenuItem alloc] initWithTitle:diffTitle
 													  action:isHead ? nil : @selector(diffFilesAction:)
 											   keyEquivalent:@""];
@@ -715,8 +724,9 @@
 - (IBAction) createBranch:(id)sender
 {
 	PBGitRef *currentRef = [repository.currentBranch ref];
-
-	if (!selectedCommit || [selectedCommit hasRef:currentRef])
+	
+	PBGitCommit *selectedCommit = self.selectedCommits.firstObject;
+	if (!selectedCommits.firstObject || [selectedCommit hasRef:currentRef])
 		[PBCreateBranchSheet beginCreateBranchSheetAtRefish:currentRef inRepository:self.repository];
 	else
 		[PBCreateBranchSheet beginCreateBranchSheetAtRefish:selectedCommit inRepository:self.repository];
@@ -724,6 +734,7 @@
 
 - (IBAction) createTag:(id)sender
 {
+	PBGitCommit *selectedCommit = self.selectedCommits.firstObject;
 	if (!selectedCommit)
 		[PBCreateTagSheet beginCreateTagSheetAtRefish:[repository.currentBranch ref] inRepository:repository];
 	else
@@ -737,18 +748,21 @@
 
 - (IBAction) merge:(id)sender
 {
+	PBGitCommit *selectedCommit = self.selectedCommits.firstObject;
 	if (selectedCommit)
 		[repository mergeWithRefish:selectedCommit];
 }
 
 - (IBAction) cherryPick:(id)sender
 {
+	PBGitCommit *selectedCommit = self.selectedCommits.firstObject;
 	if (selectedCommit)
 		[repository cherryPickRefish:selectedCommit];
 }
 
 - (IBAction) rebase:(id)sender
 {
+	PBGitCommit *selectedCommit = self.selectedCommits.firstObject;
 	if (selectedCommit)
 		[repository rebaseBranch:nil onRefish:selectedCommit];
 }

Classes/Controllers/PBRefController.h
@@ -1,5 +1,5 @@
 //
-//  PBLabelController.h
+//  PBRefController.h
 //  GitX
 //
 //  Created by Pieter de Bie on 21-10-08.
@@ -39,10 +39,5 @@
 - (void) diffWithHEAD:(PBRefMenuItem *)sender;
 - (void) createTag:(PBRefMenuItem *)sender;
 - (void) showTagInfoSheet:(PBRefMenuItem *)sender;
-
-- (NSArray *) menuItemsForRef:(PBGitRef *)ref;
-- (NSArray *) menuItemsForCommit:(PBGitCommit *)commit;
-- (NSArray *)menuItemsForRow:(NSInteger)rowIndex;
-
 
 @end

Classes/Controllers/PBWebHistoryController.h
@@ -25,8 +25,8 @@
 	NSString* diff;
 }
 
-- (void) changeContentTo: (PBGitCommit *) content;
 - (void) sendKey: (NSString*) key;
 
 @property (readonly) NSString* diff;
+
 @end

Classes/Controllers/PBWebHistoryController.m
@@ -21,13 +21,13 @@
 	startFile = @"history";
 	repository = historyController.repository;
 	[super awakeFromNib];
-	[historyController addObserver:self forKeyPath:@"webCommit" options:0 context:@"ChangedCommit"];
+	[historyController addObserver:self forKeyPath:@"webCommits" options:0 context:@"ChangedCommit"];
 }
 
 - (void)closeView
 {
 	[[self script] setValue:nil forKey:@"commit"];
-	[historyController removeObserver:self forKeyPath:@"webCommit"];
+	[historyController removeObserver:self forKeyPath:@"webCommits"];
 
 	[super closeView];
 }
@@ -35,46 +35,67 @@
 - (void) didLoad
 {
 	currentOID = nil;
-	[self changeContentTo: historyController.webCommit];
+	[self changeContentTo:historyController.webCommits];
 }
 
 - (void) observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
     if ([(__bridge NSString *)context isEqualToString: @"ChangedCommit"])
-		[self changeContentTo: historyController.webCommit];
+		[self changeContentTo:historyController.webCommits];
 	else
 		[super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
 }
 
-- (void) changeContentTo: (PBGitCommit *) content
+- (void) changeContentTo:(NSArray<PBGitCommit *> *)commits
 {
-	if (content == nil || !finishedLoading)
+	if (commits == nil || commits.count == 0 || !finishedLoading) {
 		return;
+	}
+	
+	if (commits.count == 1) {
+		[self changeContentToCommit:commits.firstObject];
+	}
+	else {
+		[self changeContentToMultipleSelectionMessage];
+	}
+}
 
-	// The sha is the same, but refs may have changed.. reload it lazy
-	if ([currentOID isEqual:content.OID])
+- (void) changeContentToMultipleSelectionMessage {
+	NSArray *arguments = @[
+			@[NSLocalizedString(@"Multiple commits are selected.", @"Multiple selection Message: Title"),
+			  NSLocalizedString(@"Use the Copy command to copy their information.", @"Multiple selection Message: Copy Command"),
+			  NSLocalizedString(@"Or select a single commit to see its diff.", @"Multiple selection Message: Diff Hint")
+			  ]];
+	[[self script] callWebScriptMethod:@"showMultipleSelectionMessage" withArguments:arguments];
+}
+
+- (void) changeContentToCommit:(PBGitCommit *)commit
+{
+	// The sha is the same, but refs may have changed. reload it lazy
+	if ([currentOID isEqual:commit.OID])
 	{
 		[[self script] callWebScriptMethod:@"reload" withArguments: nil];
 		return;
 	}
 
-	NSArray *arguments = [NSArray arrayWithObjects:content, [[[historyController repository] headRef] simpleRef], nil];
+	NSArray *arguments = [NSArray arrayWithObjects:commit, [[[historyController repository] headRef] simpleRef], nil];
 	id scriptResult = [[self script] callWebScriptMethod:@"loadCommit" withArguments: arguments];
 	if (!scriptResult) {
 		// the web view is not really ready for scripting???
-		[self performSelector:_cmd withObject:content afterDelay:0.05];
+		[self performSelector:_cmd withObject:commit afterDelay:0.05];
 		return;
 	}
-	currentOID = content.OID;
+	currentOID = commit.OID;
 
 	// Now we load the extended details. We used to do this in a separate thread,
 	// but this caused some funny behaviour because NSTask's and NSThread's don't really
 	// like each other. Instead, just do it async.
 
 	NSMutableArray *taskArguments = [NSMutableArray arrayWithObjects:@"show", @"--numstat", @"-M", @"--summary", @"--pretty=raw", currentOID.SHA, nil];
-	if (![PBGitDefaults showWhitespaceDifferences])
+	if (![PBGitDefaults showWhitespaceDifferences]) {
 		[taskArguments insertObject:@"-w" atIndex:1];
-
+	}
+	
 	NSFileHandle *handle = [repository handleForArguments:taskArguments];
 	NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
 	// Remove notification, in case we have another one running
@@ -161,8 +182,7 @@ contextMenuItemsForElement:(NSDictionary *)element
 		// Every ref has a class name of 'refs' and some other class. We check on that to see if we pressed on a ref.
 		if ([[node className] hasPrefix:@"refs "]) {
 			NSString *selectedRefString = [[[node childNodes] item:0] textContent];
-			for (PBGitRef *ref in historyController.webCommit.refs)
-			{
+			for (PBGitRef *ref in historyController.webCommits.firstObject.refs) {
 				if ([[ref shortName] isEqualToString:selectedRefString])
 					return [contextMenuDelegate menuItemsForRef:ref];
 			}

Classes/PBCommitList.m
@@ -57,6 +57,17 @@
 	[controller copyCommitSHA];
 }
 
+- (void) copyShortName:(id)sender
+{
+	[controller copyCommitShortName];
+}
+
+- (void) copyPatch:(id)sender
+{
+	[controller copyCommitPatch];
+}
+
+
 // !!! Andre Berg 20100330: Used from -scrollSelectionToTopOfViewFrom: of PBGitHistoryController
 // so that when the history controller udpates the branch filter the origin of the superview gets
 // shifted into multiples of the row height. Otherwise the top selected row will always be off by

Classes/PBRefContextDelegate.h
@@ -1,5 +1,5 @@
 //
-//  PBRefContextDelegate.m
+//  PBRefContextDelegate.h
 //  GitX
 //
 //  Created by Pieter de Bie on 01-11-08.
@@ -13,6 +13,6 @@
 
 @protocol PBRefContextDelegate
 - (NSArray *) menuItemsForRef:(PBGitRef *)ref;
-- (NSArray *) menuItemsForCommit:(PBGitCommit *)commit;
-- (NSArray *)menuItemsForRow:(NSInteger)rowIndex;
+- (NSArray *) menuItemsForCommits:(NSArray<PBGitCommit *> *)commits;
+- (NSArray *) menuItemsForRow:(NSInteger)rowIndex;
 @end

commit 3de28fb6e6b790490e0cde37c725dee0c80de37c

Fix exception thrown when pushing a ref
Introduced by an oversight in the commit 4735b1b2956f6bda4cc0610005d552ad3b4a851e
»Improve handling of multiple item selections«

Classes/Controllers/PBRefController.m
@@ -111,8 +111,7 @@
 
 - (void) pushUpdatesToRemote:(PBRefMenuItem *)sender
 {
-	PBGitRef *remoteRef = [(PBGitRef *)[sender refishs] remoteRef];
-
+	PBGitRef *remoteRef = [(PBGitRef *)sender.refishs.firstObject remoteRef];
 	[self showConfirmPushRefSheet:nil remote:remoteRef];
 }
 