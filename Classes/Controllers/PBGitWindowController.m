pieter/gitx

commit 78734e3d4a2601a87ff29d013c2ff14538a44115

Make the main controller the owner of the main controllers

Classes/Controllers/PBGitWindowController.m
@@ -70,10 +70,10 @@
 - (BOOL)validateMenuItem:(NSMenuItem *)menuItem
 {
 	if ([menuItem action] == @selector(showCommitView:)) {
-		[menuItem setState:(contentController == sidebarController.commitViewController) ? YES : NO];
+		[menuItem setState:(contentController == _commitViewController) ? YES : NO];
 		return ![self.repository isBareRepository];
 	} else if ([menuItem action] == @selector(showHistoryView:)) {
-		[menuItem setState:(contentController != sidebarController.commitViewController) ? YES : NO];
+		[menuItem setState:(contentController != _commitViewController) ? YES : NO];
 		return ![self.repository isBareRepository];
 	} else if (menuItem.action == @selector(fetchRemote:)) {
 		return [self validateMenuItem:menuItem remoteTitle:@"Fetch “%@”" plainTitle:@"Fetch"];
@@ -117,6 +117,8 @@
 	[[self window] setRepresentedURL:self.repository.workingDirectoryURL];
 
 	sidebarController = [[PBGitSidebarController alloc] initWithRepository:self.repository superController:self];
+	_historyViewController = [[PBGitHistoryController alloc] initWithRepository:self.repository superController:self];
+	_commitViewController = [[PBGitCommitController alloc] initWithRepository:self.repository superController:self];
 
 	[[sidebarController view] setFrame:[sourceSplitView bounds]];
 	[sourceSplitView addSubview:[sidebarController view]];
@@ -172,7 +174,7 @@
 	 completionHandler:^(id  _Nonnull sheet, NSModalResponse returnCode) {
 		 if (returnCode != NSModalResponseOK) return;
 
-		 [sidebarController.commitViewController forceCommit:self];
+		 [_commitViewController forceCommit:self];
 	 }];
 }
 
@@ -424,7 +426,7 @@
 	if ([types indexOfObject:kGitXCommitType] == NSNotFound)
 		return nil;
 
-	return sidebarController.historyViewController.selectedCommits.firstObject;
+	return _historyViewController.selectedCommits.firstObject;
 }
 
 - (PBSourceViewItem *) selectedItem {
@@ -733,7 +735,7 @@
 	/* WIP: must check */
 	id <PBGitRefish> refish = [self refishForSender:sender refishTypes:nil];
 	if (!refish) {
-		PBGitCommit *selectedCommit = sidebarController.historyViewController.selectedCommits.firstObject;
+		PBGitCommit *selectedCommit = _historyViewController.selectedCommits.firstObject;
 		if (!selectedCommit || [selectedCommit hasRef:currentRef]) {
 			refish = currentRef;
 		} else {
@@ -768,7 +770,7 @@
 	/* WIP: must check */
 	id <PBGitRefish> refish = [self refishForSender:sender refishTypes:nil];
 	if (!refish) {
-		PBGitCommit *selectedCommit = sidebarController.historyViewController.selectedCommits.firstObject;
+		PBGitCommit *selectedCommit = _historyViewController.selectedCommits.firstObject;
 		if (selectedCommit)
 			refish = selectedCommit;
 		else

Classes/Controllers/PBGitSidebarController.h
@@ -27,9 +27,6 @@
 	PBSourceViewItem *stage;
 
 	PBSourceViewItem *branches, *remotes, *tags, *others, *submodules, *stashes;
-
-	PBGitHistoryController *historyViewController;
-	PBGitCommitController *commitViewController;
 }
 
 - (void) selectStage;
@@ -44,7 +41,5 @@
 @property(readonly) PBSourceViewItem *remotes;
 @property(readonly) NSOutlineView *sourceView;
 @property(readonly) NSView *sourceListControlsView;
-@property(readonly) PBGitHistoryController *historyViewController;
-@property(readonly) PBGitCommitController *commitViewController;
 
 @end

Classes/Controllers/PBGitSidebarController.m
@@ -35,8 +35,6 @@
 @synthesize remotes;
 @synthesize sourceView;
 @synthesize sourceListControlsView;
-@synthesize historyViewController;
-@synthesize commitViewController;
 
 - (id)initWithRepository:(PBGitRepository *)theRepository superController:(PBGitWindowController *)controller
 {
@@ -52,9 +50,6 @@
 	[super awakeFromNib];
 	window.contentView = self.view;
 	[self populateList];
-
-	historyViewController = [[PBGitHistoryController alloc] initWithRepository:repository superController:superController];
-	commitViewController = [[PBGitCommitController alloc] initWithRepository:repository superController:superController];
 
 	[repository addObserver:self forKeyPath:@"currentBranch" options:0 context:@"currentBranchChange"];
 	[repository addObserver:self forKeyPath:@"branches" options:(NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew) context:@"branchesModified"];
@@ -83,9 +78,6 @@
 
 - (void)closeView
 {
-	[historyViewController closeView];
-	[commitViewController closeView];
-
 	[repository removeObserver:self forKeyPath:@"currentBranch"];
 	[repository removeObserver:self forKeyPath:@"branches"];
 	[repository removeObserver:self forKeyPath:@"stashes"];
@@ -239,12 +231,12 @@
 	if ([item revSpecifier]) {
 		if (![repository.currentBranch isEqual:[item revSpecifier]])
 			repository.currentBranch = [item revSpecifier];
-		[superController changeContentController:historyViewController];
+		[superController changeContentController:superController.historyViewController];
 		[PBGitDefaults setShowStageView:NO];
 	}
 
 	if (item == stage) {
-		[superController changeContentController:commitViewController];
+		[superController changeContentController:superController.commitViewController];
 		[PBGitDefaults setShowStageView:YES];
 	}
 
@@ -387,7 +379,7 @@
 	if (!ref)
 		return;
 
-	for (NSMenuItem *menuItem in [historyViewController.refController menuItemsForRef:ref])
+	for (NSMenuItem *menuItem in [superController.historyViewController.refController menuItemsForRef:ref])
 		[menu addItem:menuItem];
 }
 

Classes/Controllers/PBGitWindowController.h
@@ -22,6 +22,9 @@
 	__weak PBViewController *contentController;
 
 	PBGitSidebarController *sidebarController;
+	PBGitHistoryController *_historyViewController;
+	PBGitCommitController *_commitViewController;
+
 	__weak IBOutlet NSView *sourceListControlsView;
 	__weak IBOutlet NSSplitView *splitView;
 	__weak IBOutlet NSView *sourceSplitView;
@@ -34,6 +37,8 @@
 @property (nonatomic, strong) PBGitRepository *repository;
 /* This is assign because that's what NSWindowController says :-S */
 @property (assign) PBGitRepositoryDocument *document;
+@property (readonly) PBGitHistoryController *historyViewController;
+@property (readonly) PBGitCommitController *commitViewController;
 
 - (instancetype)init;
 

commit e2b5713b6d50245aca9d66fda4719b977607c18d

Close our subview controllers when the window closes
Fixes a leaking KVO introduced in 78734e3d4a2601a87ff29d013c2ff14538a44115

Classes/Controllers/PBGitWindowController.m
@@ -63,6 +63,9 @@
 	if (sidebarController)
 		[sidebarController closeView];
 
+	[self.historyViewController closeView];
+	[self.commitViewController closeView];
+
 	if (contentController)
 		[contentController removeObserver:self forKeyPath:@"status"];
 }